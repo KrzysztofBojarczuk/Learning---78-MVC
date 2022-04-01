cb25a2582ac1bce91b7e04e8f036ef84882e887b
Krzysztof <Krzysztofbojarczuk@wp.pl>
2022-04-01 20:37:51 +0200

    Register PAge

--- RunnWebApplication1/Migrations/20220401171649_NullAdressFix.Designer.cs
+++ RunnWebApplication1/Migrations/20220401171649_NullAdressFix.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using RunnWebApplication1.Data;
 
@@ -11,9 +12,10 @@ using RunnWebApplication1.Data;
 namespace RunnWebApplication1.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220401171649_NullAdressFix")]
+    partial class NullAdressFix
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- RunnWebApplication1/Models/UserRoles.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace RunnWebApplication1.Models
-{
-    public class UserRoles
-    {
-        public const string Admin = "admin";
-        public const string User = "user";
-    }
-}
